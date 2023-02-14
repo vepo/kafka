5006d4247bd502d13f8e877f4e20176b0d4284bd
Victor Osório <vepo@tutanota.com>
2023-02-13 23:16:01 -0300

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# kafka
+# Kafka
+
+Kafka docker image.
+
+Any config value can be set by using the `KAFKA_` prefix.
---
+++ start-kafka.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+PROPERTIES=./kafka/config/kraft/server.properties
+
+for k_var in $(env | grep ^KAFKA_); do
+    key="${k_var%=*}"
+    key=`echo ${key#"KAFKA_"} | tr "[:upper:]" "[:lower:]" | tr '_' '.'`
+    value="${k_var#*=}"
+
+    if ! grep -R "^[#]*\s*${key}=.*" $PROPERTIES > /dev/null; then
+        echo "APPENDING because '${key}' not found"
+        echo "$key=$value" >> $PROPERTIES
+    else
+        echo "SETTING because '${key}' found already"
+        sed -ir "s/^[#]*\s*${key}=.*/$key=$value/" $PROPERTIES
+    fi
+done
+
+if [[ ! -f /tmp/kraft-combined-logs/meta.properties ]]; then
+    rand=`./kafka/bin/kafka-storage.sh random-uuid`
+    ./kafka/bin/kafka-storage.sh format -t $rand -c $PROPERTIES
+fi
+
+# Run Kafka
+./kafka/bin/kafka-server-start.sh $PROPERTIES
